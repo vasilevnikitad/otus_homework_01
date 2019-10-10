8ac92863c41126327b1680cffdeddbfcd214a3ec
Nikita Vasilev <nvasilev@oktetlabs.ru>
2019-10-10 16:18:39 +0300

    replace build num logging with version logging

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 int main()
 {
   boost::log::add_console_log(std::cout, boost::log::keywords::format = "%Message%");
-  BOOST_LOG_TRIVIAL(info) << "build " << project_info::version::get_build_num();
+  BOOST_LOG_TRIVIAL(info) << "build " << project_info::version;
   BOOST_LOG_TRIVIAL(info) << "Hello, world!";
   return 0;
 }
